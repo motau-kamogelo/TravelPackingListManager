830a0bcf8de3a2eda228ccb9d1f51726113db8d4
Kamogelo Motau <127486770+motau-kamogelo@users.noreply.github.com>
2025-11-04 12:39:14 +0200

    Testing repo

--- app/src/main/java/com/example/travelpackinglistmanager/MainActivity.kt
+++ app/src/main/java/com/example/travelpackinglistmanager/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TravelPackingListManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android 2025",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
